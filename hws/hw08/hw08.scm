57ad7ae1faabe6a7db1c8a6e21d54240f997ff6d
dyute <rivershao@outlook.com>
2022-07-22 21:27:38 +0800

    hw08 finished

---
+++ hws/hw08/hw08.scm
@@ -0,0 +1,27 @@
+(define (my-filter func lst)
+  (if (null? lst)
+      nil
+      (if (func (car lst))
+          (cons (car lst) (my-filter func (cdr lst)))
+          (my-filter func (cdr lst)))))
+
+(define (interleave s1 s2)
+  (if (null? s1)
+      s2
+      (if (null? s2)
+          s1
+          (cons (car s1) (interleave s2 (cdr s1))))))
+
+(define (accumulate merger start n term)
+  (if (= n 0)
+      start
+      (merger (term n)
+              (accumulate merger start (- n 1) term))))
+
+(define (no-repeats lst)
+  (if (null? lst)
+      nil
+      (cons (car lst)
+            (no-repeats
+             (my-filter (lambda (x) (not (= (car lst) x)))
+                        (cdr lst))))))
